5e725bc9a26c5fe4503e4ed326468294eae08c74
tobanigj <86021593+tobanigj@users.noreply.github.com>
2024-11-21 23:23:59 -0500

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
+
+if __name__ == "__main__":
+    print("Calculadora Básica")
